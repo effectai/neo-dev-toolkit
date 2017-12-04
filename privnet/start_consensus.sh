b38fdf3ac5ac673dba6ca5fef5ace833e89559d4
Jesse Eisses <jesse.eisses@gmail.com>
2017-12-04 23:18:03 +0100

    Update to latest version
    - use wallets from docker-privnet-docker
    - expose rpc and p2p ports

--- privnet/start_consensus.sh
+++ privnet/start_consensus.sh
@@ -3,7 +3,7 @@ spawn dotnet neo-cli.dll /rpc
 expect "neo>"
 send "open wallet wallet.db3\n"
 expect "password:"
-send "test\n"
+send "$env(PASS)\n"
 expect "neo>"
 send "list key\n"
 send "start consensus\n"
